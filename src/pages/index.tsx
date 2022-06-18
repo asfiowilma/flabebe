4830cd539f355369f67210296cac6f9a9878fc61
asfiowilma <asfiolithaw@yahoo.co.id>
2022-06-19 02:36:26 +0700

    🍱 add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 import Image from "next/image";
 import { Suspense } from "react";
 import TrakteerButton from "../components/trakteer/TrakteerButton";
@@ -14,6 +15,14 @@ const Home: NextPage = () => {
   return (
     <div className="flex flex-col gap-4 justify-center items-center h-screen bg-[url('/images/bg.svg')] bg-no-repeat bg-cover text-white p-4">
       <TrakteerProvider>
+        <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/logo.png"
+          />
+        </Head>
         <Image src="/images/logo.png" alt="logo" width={80} height={80} />
         <Suspense>
           <CreateLinkForm />
